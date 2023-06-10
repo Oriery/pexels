163a1650934690a287c56f2dc9f2f7ce341b58e6
Oriery <panev291002@gmail.com>
2023-06-10 15:16:04 +0300

    responsive quantity of columns

--- src/components/PicturesList/index.tsx
+++ src/components/PicturesList/index.tsx
@@ -1,17 +1,30 @@
+import { useEffect, useRef, useState } from "react";
 import Picture from "../Picture";
 import { Photo } from 'pexels';
 
 const minHeightsOfMocks = Array.from({ length: 11 }, () => Math.round(200 + Math.random() * 200));
 
 function PicturesList({images} : { images: Photo[] }) {
-  const COLUMNS = 3;
+  const [columnsNumber, setColumnsNumber] = useState(window.innerWidth < 768 ? 2 : 3);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        setColumnsNumber(2);
+      } else {
+        setColumnsNumber(3);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   type ImageWithEl = { el: JSX.Element, picture: Photo | null };
   
   const imagesElems : ImageWithEl[] = images.length ? images.map((image, index) => {
     return {
       el: (
-        <Picture image={image} key={image.id} numberOfColumns={COLUMNS} mockMinHeight={minHeightsOfMocks[index % minHeightsOfMocks.length]} />
+        <Picture image={image} key={image.id} numberOfColumns={columnsNumber} mockMinHeight={minHeightsOfMocks[index % minHeightsOfMocks.length]} />
       ), 
       picture: image
     };
@@ -23,6 +36,7 @@ function PicturesList({images} : { images: Photo[] }) {
       picture: null
     };
   });
+
 
   const columnsOfImages = imagesElems.reduce((acc : {images: JSX.Element[], colHeight: number}[], imageWithEl, index) => {
     // column with min height
@@ -37,7 +51,8 @@ function PicturesList({images} : { images: Photo[] }) {
 
     acc[column].images.push(imageWithEl.el);
     return acc;
-  }, Array.from({ length: COLUMNS }, () => ({ images: [], colHeight: 0 })));
+  }, Array.from({ length: columnsNumber }, () => ({ images: [], colHeight: 0 })));
+
 
   const columnsOfImagesElems = columnsOfImages.map((column, index) => {
     return (
@@ -45,11 +60,11 @@ function PicturesList({images} : { images: Photo[] }) {
         {column.images}
       </div>
     )
-  })
+  });
 
   return (
-    <div className="m-4 sm:m-8">
-      <div className="flex flex-row h-10 w-full justify-between">
+    <div className="PictureList m-4 md:m-8">
+      <div className="flex flex-col md:flex-row h-10 w-full justify-between">
         <h1 className="text-2xl h-full flex items-center">Бесплатные стоковые фото</h1>
         <p className="whitespace-nowrap h-full flex items-center text-xs">All photos are provided by&nbsp;
           <a href="https://www.pexels.com" className="text-[#07a081]">
@@ -57,7 +72,7 @@ function PicturesList({images} : { images: Photo[] }) {
           </a>
         </p>
       </div>
-      <div className="grid grid-cols-3 gap-4 sm:gap-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-8 mt-4">
         {columnsOfImagesElems}
       </div>
     </div>
