bd95baff0363a4a304a4ba3c7d8e7cc2c7c9325e
Oriery <panev291002@gmail.com>
2023-06-11 23:14:53 +0300

    search bar inside of navbar; adaptive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import logoBlack from './logo-black.svg';
+import logoSmall from './logo-small.svg';
+import menu from './menu.png';
 import SearchBar from '../SearchBar';
 import { useEffect, useState } from 'react';
 import { Photo } from 'pexels';
@@ -38,19 +40,42 @@ function Header({onSearch, randomNatureImage} : {onSearch: (searchQuery: string)
         }}
       >
         <div className='mx-auto max-w-[1216px] 2xl:max-w-[1600px] w-full h-full flex flex-row space-x-4 justify-between'>
-          <a href='/' className='flex flex-row'>
-            {
-            !headerIsOutOfView ? 
-              <img src={logo} className='App-logo' alt='logo' /> :
-              <img src={logoBlack} className='App-logo-black' alt='logo' /> 
-            }
-          </a>
-          <div className='flex space-x-8 py-2'>
+          <div className='flex flex-row h-full w-full items-center py-2'>
+            <a href='/' className='hidden lg:flex flex-row flex-none h-full'>
+              {
+              !headerIsOutOfView ? 
+                <img src={logo} className='h-full' alt='logo' /> :
+                <img src={logoBlack} className='h-full' alt='logo'/> 
+              }
+            </a>
+            <a href='/' className='flex lg:hidden flex-row flex-none h-full'>
+              <img src={logoSmall} className='h-full' alt='logo' />
+            </a>
+            { headerIsOutOfView && (
+              <div className='flex items-center w-full xl:max-w-[420px] ml-4 h-full'>
+                <SearchBar onSearch={search} />
+              </div>
+            )}
+          </div>
+          <div className='space-x-8 py-2 flex-none hidden lg:flex'>
             <a className='flex items-center' href='/'>Поиск фото</a>
             <a className='flex items-center' href='/'>Лицензия</a>
             <a className='flex items-center' href='/'>Загрузка</a>
             <a className='flex items-center' href='/'>•••</a>
             <a className={'flex items-center bg-white rounded-md px-4 text-black' + (headerIsOutOfView ? ' bg-[#07a081] text-white' : '')} href='/'>Регистрация</a>
+          </div>
+          <div className='flex flex-row h-full space-x-8 flex lg:hidden items-center'>
+            <div className='space-x-8 h-full py-2 flex-none flex'>
+              <a className={'flex items-center bg-white rounded-md px-4 text-black' + (headerIsOutOfView ? ' bg-[#07a081] text-white' : '')} href='/'>Регистрация</a>
+            </div>
+            <a className='flex items-center w-6 h-6'
+              style={{
+                filter: headerIsOutOfView ? 'invert(0)' : 'invert(1)',
+              }}
+              href='/'
+            >
+              <img src={menu} alt='menu' />
+            </a>
           </div>
         </div>
       </nav>
--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -10,12 +10,14 @@ function SearchBar({ onSearch } : { onSearch : (searchQuery : string) => void })
   }
 
   return (
-    <form className="search-bar flex flex-row place-content-stretch w-full bg-white rounded-md" onSubmit={onFormSubmit}>
-      <div className="flex items-center text-black px-2 m-4">
+    <form className="search-bar flex flex-row justify-between items-center w-full bg-gray-100 rounded-md h-full" onSubmit={onFormSubmit}>
+      <div className="flex items-center text-black px-7 py-1 my-2 pr-6 mr-1" style={{
+        boxShadow: '1px 0 0 #e7e7e7',
+      }}>
         Фото
       </div>
-      <input className="w-full px-2 text-black focus:outline focus:outline-0" type="text" placeholder="Поиск бесплатных изображений" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}  />
-      <button className="flex flex-row place-content-center place-items-center p-2 m-2 h-10 aspect-square">
+      <input className="w-full px-2 bg-gray-100 text-black focus:outline focus:outline-0" type="text" placeholder="Поиск бесплатных изображений" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}  />
+      <button className="flex flex-row place-content-center place-items-center p-2 m-1 h-10 aspect-square">
         <img src={search} alt='search' />
       </button>
     </form>
