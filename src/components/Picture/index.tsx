65c5ae26ad4fd76db51502ebf2604dc285a84f7d
Oriery <panev291002@gmail.com>
2023-06-12 20:04:45 +0300

    Picture element: layout

--- src/components/Picture/index.tsx
+++ src/components/Picture/index.tsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Photo } from 'pexels';
 import './Picture.css';
+import heart from './heart.png'
+import bookmark from "./bookmark.png";
+import download from "./download.png";
 
 interface PictureProps {
   image?: Photo;
@@ -52,14 +55,38 @@ class Picture extends Component<PictureProps, PictureState> {
           paddingBottom: isLoaded ? undefined : (image ? `${image.height / image.width * 100}%` : undefined)
         }}>
           {!this.props.isMock ? (
-            <img className="w-full" 
-            alt={image!.alt || undefined} 
-            src={image!.src.large}
-            srcSet={SIZES.reduce((acc, px) => acc + `${image!.src.original}?auto=compress&cs=tinysrgb&w=${px} ${px}w, `, '').slice(0, -2)}
-            loading="lazy"
-            onLoad={this.handleImageLoad}
-            sizes={`${SIZES.reduce((acc, px) => acc + `(max-width: ${px * numberOfColumns!}px) ${px}px, `, '')} 1600px`}
-            />
+            <div className='relative'>
+              <img className="w-full" 
+              alt={image!.alt || undefined} 
+              src={image!.src.large}
+              srcSet={SIZES.reduce((acc, px) => acc + `${image!.src.original}?auto=compress&cs=tinysrgb&w=${px} ${px}w, `, '').slice(0, -2)}
+              loading="lazy"
+              onLoad={this.handleImageLoad}
+              sizes={`${SIZES.reduce((acc, px) => acc + `(max-width: ${px * numberOfColumns!}px) ${px}px, `, '')} 1600px`}
+              />
+              <div className='absolute top-0 left-0 h-full w-full flex flex-col justify-between p-2 opacity-0 hover:opacity-100'
+                style={{
+                  background: 'linear-gradient(0deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0) 30%, rgba(0,0,0,0) 70%, rgba(0,0,0,0.3) 100%)'
+                }}
+              >
+                <div className='flex flex-row w-full space-x-1 justify-end'>
+                  <div className='bg-white rounded-lg p-3 w-10 h-10 flex items-center'>
+                    <img src={bookmark} alt="bookmark" className='' />
+                  </div>
+                  <div className='bg-white rounded-lg p-3 w-10 h-10 flex items-center'>
+                    <img src={heart} alt="like" className='' />
+                  </div>
+                </div>
+                <div className='flex flex-row w-full space-x-1 justify-between'>
+                  <div className='flex items-center px-2'>
+                    <a className='text-md text-white font-semibold' href={image!.photographer_url} target='_blank' rel="noreferrer">{image!.photographer}</a>
+                  </div>
+                  <div className='bg-white rounded-lg p-3 w-10 h-10 flex items-center'>
+                    <img src={download} alt="download" className='' />
+                  </div>
+                </div>
+              </div>
+            </div>
           ) : undefined }
         </div>
       </div>
