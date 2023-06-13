a6608c7825c92d01013f3e37b600ce937c35ea23
Oriery <panev291002@gmail.com>
2023-06-13 16:14:29 +0300

    SearchBar into focus

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,11 @@
 import search from './search.svg'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 
 function SearchBar({ onSearch } : { onSearch: (query : string, goingToMainPage? : boolean) => void }) {
   let [searchQuery, setSearchQuery] = useState(useParams().query || '')
   let navigate = useNavigate()
+  const inputRef = useRef<HTMLInputElement>(null);
 
   function handleSubmit(e : React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
@@ -18,6 +19,12 @@ function SearchBar({ onSearch } : { onSearch: (query : string, goingToMainPage? 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
+
   return (
     <form className="search-bar flex flex-row justify-between items-center w-full bg-gray-100 rounded-md h-full" onSubmit={handleSubmit}>
       <div className="flex items-center text-black px-7 py-1 my-2 pr-6 mr-1" style={{
@@ -25,7 +32,14 @@ function SearchBar({ onSearch } : { onSearch: (query : string, goingToMainPage? 
       }}>
         Фото
       </div>
-      <input className="w-full px-2 bg-gray-100 text-black focus:outline focus:outline-0" type="text" placeholder="Поиск бесплатных изображений" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}  />
+      <input 
+        className="w-full px-2 bg-gray-100 text-black focus:outline focus:outline-0" 
+        type="text" 
+        placeholder="Поиск бесплатных изображений" 
+        value={searchQuery} 
+        onChange={(e) => setSearchQuery(e.target.value)} 
+        ref={inputRef}
+      />
       <button type="submit" className="flex flex-row place-content-center place-items-center p-2 m-1 h-10 aspect-square">
         <img src={search} alt='search' />
       </button>
